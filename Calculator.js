17acfe6ddc4c3f7bae13704a3c4e81fefc937da2
nevertoolateforITgit config --global user.name :nevertoolateforIT <nkandratenia@gmail.com>
2022-05-06 18:42:03 -0400

    first commit

---
+++ Calculator.js
@@ -0,0 +1,28 @@
+  
+  let op;
+  function func(){
+    let result;
+      let Num1= Number(document.getElementById("Num1").value);
+      let Num2= Number(document.getElementById("Num2").value);
+      switch (op){
+          case'+':
+          result=Num1+Num2;
+          break;
+          case '-':
+              result=Num1-Num2;
+              break;
+          case'*':
+          result=Num1*Num2;
+          break;
+          case'/':
+          result=Num1/Num2;
+          break;
+      }
+document.getElementById("result").innerHTML=result;
+  }
+  
+  
+  
+  
+
+         
