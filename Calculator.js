2c463a1e56e4c8ffbab5e92d6955cbd5b8ef0878
nevertoolateforITgit config --global user.name :nevertoolateforIT <nkandratenia@gmail.com>
2022-05-12 14:33:29 -0400

    added if statement to alert division by 0

--- Calculator.js
+++ Calculator.js
@@ -4,6 +4,7 @@
     let result;
       let Num1= Number(document.getElementById("Num1").value);
       let Num2= Number(document.getElementById("Num2").value);
+      
       switch (op){
           case'+':
           result=Num1+Num2;
@@ -15,11 +16,19 @@
           result=Num1*Num2;
           break;
           case'/':
-          result=Num1/Num2;
+          if (op =='/'){
+            if (Num2==0){
+              alert("Hа ноль делить нельзя!");
+              return;
+            }
+            result=Num1/Num2;
+          }
           break;
       }
 document.getElementById("result").innerHTML=result;
   }
+  
+  
   
   
   
